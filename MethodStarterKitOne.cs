a63efa8e91c0a8696cecdd66b5c2bdeb0d8e68fe
Shafqat <64027495+Neuroplastic1@users.noreply.github.com>
2021-09-13 11:49:10 -0400

    Defining methods
    In C#, it’s convention to use PascalCase to name our method. The name starts with an uppercase letter and each word following begins with an uppercase as well. It’s not required in C#, but it makes our code easier to read for other developers.
    
    The body of our method goes between the curly braces: { }. Whenever the method is called, the code in the body is executed.
    We can also include default parameters for methods.(line 28)

--- MethodStarterKitOne.cs
+++ MethodStarterKitOne.cs
@@ -9,17 +9,25 @@ namespace DefineAMethod
     //Main() is a method. Every time we run the code, the Main() method is executed.
     
     static void Main(string[] args)
-    {
-      //Call the external method in Main() with appropriate param and run the code
-      VisitPlanets(3);
-      VisitPlanets(4);
-      VisitPlanets(5);
-    }
+     {
+       //Call the external methods in Main() with appropriate param and run the code
+       VisitPlanets(3);
+       VisitPlanets(4);
+       VisitPlanets(5);
+       //testing default parameters
+       YourMethodName("I'm hungry", "!"); // prints "I'm hungry!"
+       YourMethodName("I'm hungry");  // prints "I'm hungry."
+     }
     //Define a method named VisitPlanets() outside of the Main() method and run the code.
     //Since Main() is already a method, we’ll define our own methods outside of Main().
     static void VisitPlanets(int numberOfPlanets)
-    {
-      Console.WriteLine($"You visited {numberOfPlanets} new planets...");
-    }
+     {
+       Console.WriteLine($"You visited {numberOfPlanets} new planets...");
+     }
+    //Default parameters for 
+    static void YourMethodName(string message, string punctuation = ".")
+     {
+       Console.WriteLine(message + punctuation);
+     }
   }
 }
